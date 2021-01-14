a434a4ca1e9a508e5944db12f18d7f626f1a5e67
Carlos2MorenoM <c.morenomontesdeoca@gmail.com>
2021-01-14 15:53:05 +0100

    code refactor

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,91 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid">
+    <div class="text-center">
+      <h1>Aplicación de Gestión de Afiliados</h1>
+    <p> Construido con Nest.js, Vue.js y MongoDB</p>
+
+    <div v-if="customers.length === 0">
+      <h2> No hay afiliados todavía (: </h2>
+    </div>
+
   </div>
+
+  <div class="">
+    <table class="table table-bordered">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">DNI</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Apellidos</th>
+          <th scope="col">Mail</th>
+          <th scope="col">Teléfono</th>
+          <th scope="col">Acciones</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="customer in customers" :key="customer._id">
+          <td>{{ customer.DNI }}</td>
+          <td>{{ customer.nombre }}</td>
+          <td>{{ customer.appellidos }}</td>
+          <td>{{ customer.mail }}</td>
+          <td>{{ customer.telefono }}</td>
+          <td>
+
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group" style="margin-bottom: 20px;">
+
+                <router-link :to="{name: 'Edit', params: {id: customer._id}}" class ="btn btn-sm btn-outline-secondary"> Editar Afiliado </router-link>       
+
+                 <button class="btn btn-sm btn-outline-secondary" v-on:click="deleteCustomer(customer._id)">Eliminar Afiliado</button>
+
+                </div>
+
+            </div>
+
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+  </div>
+  
+
+
 </template>
+
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { server } from "../helper";
+
+import axios from "axios";
+
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      customers: []
+    };
+  },
+  created() {
+    this.fetchCustomers();
+  },
+  methods: {
+    fetchCustomers() {
+      axios
+        .get(`${server.baseURL}/customer/customers`)
+        .then(data => (this.customers = data.data));
+    },
+    deleteCustomer(id) {
+      axios
+        .delete(`${server.baseURL}/customer/delete?customerID=${id}`)
+        .then(data => {
+          console.log(data);
+          window.location.reload();
+        });
+    }
   }
-}
+};
 </script>
